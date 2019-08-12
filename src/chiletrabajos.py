b1110b3fc2c2b3cd2479e1743cbbfffc48c23d87
nicolas-pacheco <n.pacheco.alarcon@gmail.com>
2019-08-12 01:40:21 -0400

    Importacion de librerias

--- src/chiletrabajos.py
+++ src/chiletrabajos.py
@@ -1,2 +1,8 @@
-print("hola nuevo proyecto")
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import pandas as pd
+import shutil
 
